5f33ce6f50218b029b57e5a035685a63efccb707
Matteo Pergolesi <matpergo@gmail.com>
2021-01-08 17:42:15 +0100

    Add kafka v3 Docker image.

---
+++ docker_images/microservices_scenario/kafka/v3/files/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+listener_ip_addresses=$1
+adv_listener_ip_addresses=$2
+broker_id=$3
+zookeeper_ip_address=$4
+rack_name=$5
+
+# Modify /opt/kafka/config/server.properties (lines with environment variables)
+echo "Modify /opt/kafka/config/server.properties"
+sed -i -e "s;#listeners=PLAINTEXT://:9092;listeners=$listener_ip_addresses;" /opt/kafka/config/server.properties
+sed -i -e "s;#advertised.listeners=PLAINTEXT://your.host.name:9092;advertised.listeners=$adv_listener_ip_addresses;" /opt/kafka/config/server.properties
+sed -i -e "s/broker.id=0/broker.id=$broker_id/" /opt/kafka/config/server.properties
+sed -i -e "s/zookeeper.connect=localhost:2181/zookeeper.connect=$zookeeper_ip_address:2181/" /opt/kafka/config/server.properties
+echo "broker.rack=$rack_name" | tee -a /opt/kafka/config/server.properties > /dev/null
+
+# Start Kafka
+echo "Start Kafka"
+sleep 5
+/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
